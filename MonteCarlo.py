7284946369018b8097089f76dd9922a90788c9a7
zwdnet <zwdnet@163.com>
2019-01-26 07:54:16 +0800

    完成最大最小年化收益率，最大回撤，α，β和shape指数的计算，就是不知道算对了没有。

--- MonteCarlo.py
+++ MonteCarlo.py
@@ -3,6 +3,7 @@
 
 import pandas as pd
 import datetime
+import numpy as np
 
 
 #将八位数字格式日期转化为datetime类型
@@ -27,31 +28,31 @@ def Days(year):
         return 365
 
 #计算投资策略的相关评价数据
-def Judgement(data):
+def Judgement(data, basedata):
     #1.计算年化收益率，算最大最小的吧
     maxInput = data.收益率.max()
     minInput = data.收益率.min()
-    print(maxInput, minInput)
+    #print(maxInput, minInput)
     #计算最大年化收益率
     dateend = data[data.收益率 == maxInput].日期
     datebegin = data.日期[0]
-    print(datebegin)
+    #print(datebegin)
     dateb = GetDatetime(datebegin)
     datee = GetDatetime(dateend)
     days = (datee - dateb).days
-    print(days)
+    #print(days)
     days_per_year = Days(int(dateend/10000))
     MaxRate = maxInput/days*days_per_year
     #计算最大损失率
     dateend = data[data.收益率 == minInput].日期
-    print(dateend)
+    #print(dateend)
     datee = GetDatetime(dateend)
     days = (datee - dateb).days
-    print(days)
+    #print(days)
     days_per_year = Days(int(dateend/10000))
     MinRate = minInput/days*days_per_year
-    print(MaxRate, MinRate)
-    #计算最大回撤率
+    #print(MaxRate, MinRate)
+    #2.计算最大回撤率
     i = 0
     maxRedraw = 0.0
     maxRedrawRate = 0.0
@@ -61,10 +62,41 @@ def Judgement(data):
         if RedrawRate > maxRedrawRate:
             maxRedrawRate = RedrawRate
         i += 1
-    print(maxRedrawRate)
+    #print(maxRedrawRate)
+    #3.计算beta比例
+    #用平均收益率，先计算沪深300的基准收益率,以及标准差
+    baserate = [0.0] #设第一天收益率为0
+    i = 0
+    for i in range(1, len(basedata.close)):
+        baserate.append((basedata.close[i] - basedata.close[i-1])/basedata.close[i-1])
+    mean_baserate = np.mean(baserate)
+    std_baserate = np.std(baserate)
+    #计算投资组合的平均收益率
+    mean_rate = data.收益率.mean()
+    std_rate = data.收益率.std()
+    #print(mean_rate, std_rate)
+    #计算β系数
+    #以余额宝收益率3%为无风险收益率
+    n = len(data)
+    rate_year = (data.收益率[n-1]/data.收益率[0])**(250.0/n - 1)
+    baserate_year = (basedata.close[n-1]/basedata.close[0])**(250.0/n - 1)
+    beta = (rate_year-0.03)/(baserate_year-0.03)
+    #print(beta)
+    #4.计算α系数
+    # print(rate_year-0.03, beta*(baserate_year-0.03))
+    alpha = rate_year - 0.03 - beta*(baserate_year - 0.03)
+    #print(alpha)
+    #5.计算夏普指数
+    shape = (mean_rate-0.03)/std_rate
+    #print(shape)
+    return pd.Series([MaxRate, MinRate, maxRedrawRate, beta, alpha, shape])
+    
     
 
 if __name__=="__main__":
     df_etf = pd.read_csv("total_etf.csv")
+    df_300 = pd.read_csv("300etf.csv")
     print(df_etf.head())
-    Judgement(df_etf)
+    print(df_300.head())
+    result = Judgement(df_etf, df_300)
+    print(result)
