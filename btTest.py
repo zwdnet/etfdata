82c4a29a4e8b05b00d1b7e60c20e4e7185ef1d2f
zwdnet <zwdnet@163.com>
2019-05-25 21:24:45 +0800

    照着BT文档的例子敲，并尝试将原始数据存入csv文件再读取，结果执行回测有问题，是读取的数据的index改变的原因，搜到了解决方案，解决了。将DataFrame的index重新设置为datatime格式。

--- btTest.py
+++ btTest.py
@@ -6,12 +6,30 @@
 
 import bt
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
+#输出结果
+def Output(res):
+    fig = res.plot()
+    plt.savefig("btTest.png")
+    res.display()
+    fig = res.plot_histogram()
+    plt.savefig("btHistTest.png")
+    res.plot_security_weights()
+    plt.savefig("btWeights.png")
+    
+    
 if __name__ == "__main__":
     # 获取数据
-    data = bt.get("spy, agg", start = "2010-01-01")
-    print(data.head())
+    #data = bt.get("spy, agg", start = "2010-01-01")
+    #print(data.head(), data.describe())
+    #data.to_csv("btTest.csv")
+    data2 = pd.read_csv("btTest.csv")
+    data2["Date"] = pd.to_datetime(data2["Date"])
+    data2.set_index("Date", inplace=True) 
+    print(data2.head())
+
     # 创建策略
     s = bt.Strategy("s1", 
     [bt.algos.RunMonthly(),
@@ -19,15 +37,19 @@ if __name__ == "__main__":
      bt.algos.WeighEqually(),
      bt.algos.Rebalance()])
     # 建立回测
-    test = bt.Backtest(s, data)
+    test = bt.Backtest(s, data2)
     res = bt.run(test)
     # 输出结果
-    fig = res.plot()
-    print(type(fig))
-    plt.savefig("btTest.png")
-    res.display()
-    fig = res.plot_histogram()
-    print(type(fig))
-    plt.savefig("btHistTest.png")
-    res.plot_security_weights()
-    plt.savefig("btWeights.png")
+    Output(res)
+    # 建立新的策略
+    s2 = bt.Strategy("s2",
+    [bt.algos.RunWeekly(),
+     bt.algos.SelectAll(),
+     bt.algos.WeighInvVol(),
+     bt.algos.Rebalance()])
+    # 建立回测
+    test2 = bt.Backtest(s2, data2)
+    # 执行回测，一步一步显示
+    res2 = bt.run(test, test2)
+    # 输出
+    Output(res2)
